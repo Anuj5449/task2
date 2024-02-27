039d78f844bef504bdf86a567997086744db57e7
Anuj5449 <159900001+Anuj5449@users.noreply.github.com>
2024-02-27 14:10:28 +0530

    Delete Email_Project_Task_2/task2_project directory

--- Email_Project_Task_2/task2_project/email_app/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Employee(models.Model):
-    emp_id = models.IntegerField()
-    f_name = models.CharField(max_length = 45)
-    l_name = models.CharField(max_length = 45)
-    salary = models.FloatField()
-    city = models.CharField(max_length = 45)
-     
--- Email_Project_Task_2/task2_project/email_app/urls.py
+++
@@ -1,7 +0,0 @@
-from django.urls import path
-from .views import create_emp, details
-
-urlpatterns = [
-    path('add/',create_emp),
-    path('add/<int:pk>/',details),
-]
